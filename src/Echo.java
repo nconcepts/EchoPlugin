eb75a129715ef362366e36c83a8597415dd8153b
Yannick SILVA <yannick.silva@nconcepts.fr>
2013-11-11 17:50:32 +0800

    First version of EchoPlugin for Phonegap 3.0

---
+++ src/Echo.java
@@ -0,0 +1,33 @@
+package fr.nconcepts.echo;
+ 
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+public class Echo extends CordovaPlugin {
+   public static final String ECHO = "echo";
+   
+   @Override
+   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+ 
+       try {
+          if (ECHO.equals(action)) {
+             JSONObject arg_object = args.getJSONObject(0);
+				   
+		     arg_object.getString("message")
+				   
+             callbackContext.success();
+             return true;
+          }
+          callbackContext.error("Invalid action");
+          return false;
+      } catch(Exception e) {
+          System.err.println("Exception: " + e.getMessage());
+          callbackContext.error(e.getMessage());
+          return false;
+      } 
+ 
+   }
+}
---
+++ www/echo.js
@@ -0,0 +1,15 @@
+var echo = {
+    createEvent: function(message) {
+        cordova.exec(
+            successCallback, // success callback function
+            errorCallback, // error callback function
+            'Echo', // mapped to our native Java class called "Calendar"
+            'echo', // with this action name
+            [{                  // and this array of custom arguments to create our entry
+                "message": message
+            }]
+        );
+     }
+}
+
+module.exports = echo;
